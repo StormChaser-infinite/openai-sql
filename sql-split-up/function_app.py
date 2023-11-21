487832c551a786fb833dbc0c8e8fb720b81a92d7
StormChaser-infinite <105186977+StormChaser-infinite@users.noreply.github.com>
2023-11-21 15:23:22 +1300

    updated the logic to adopt the simple SQL queries.

--- sql-split-up/function_app.py
+++ sql-split-up/function_app.py
@@ -35,7 +35,9 @@ def split_sql_input(myblob: func.InputStream):
                 counts = counts + 1
 
         if counts == 0:
-            print(f"The file {myblob.name} couldnot find any Stored Procedures, functions or views!" )
+            print(f"The file {myblob.name} could not find any new Stored Procedures, functions or views! It may be a simple SQL query." )
+            filename = myblob.name[myblob.name.find("/")+1:].split(".")[0]
+            export_txt_file(sql_lines, filename)
         elif counts == 1: 
             print(f'There is only one SP/Function/View in the SQL script, no need to split the file!')
             export_txt_file(sql_lines)  
@@ -60,14 +62,15 @@ def split_sql_input(myblob: func.InputStream):
         print(f'The SQL scripts did not split successfully because {e}.')
 
 
-def export_txt_file(sql_section: list):
+def export_txt_file(sql_section: list, file = ""):
     """Exported the split SQL script into txt files """
     try:
         for s in sql_section:
             if s.lower().strip().startswith(check_list[0]) or s.lower().strip().startswith(check_list[1]) or s.lower().strip().startswith(check_list[2]):
                 end_index = [e for e, n in enumerate(list(s)) if n == ']'][1]
                 file_name = re.sub("[\W_]+"," ", ' '.join([t.lower() for t in s[0:end_index].strip().split(" ")])).replace(" ", "_")
-                
+            
+        file_name = [file_name if file == "" else file][0]    
         output_file = file_name + '.txt'
         blobService = BlobServiceClient.from_connection_string(connectionstring)
         blob_client = blobService.get_blob_client(output_container, output_file)
