654445e70f89725f318962b0e5c3f05d6e6b87f2
StormChaser-infinite <105186977+StormChaser-infinite@users.noreply.github.com>
2023-11-22 13:32:22 +1300

    connected to OpenAI and pushed the list of questions and saved the responses into Cusmos DB.

--- connect-api/function_app.py
+++ connect-api/function_app.py
@@ -1,8 +1,16 @@
-import logging
+import azure.functions as func
 from openai import AzureOpenAI
 from azure.storage.blob import ContainerClient 
 import pymongo
 import datetime as dt
+import logging
+import json
+
+input_container = 'sql-selected'
+cosmos_db_name = "sql-model-review"
+collection_name = "kitt-collection"
+app = func.FunctionApp()
+@app.blob_trigger(arg_name="myblob", path="sql-selected/{name}",connection="blobconnectionstring") 
 
 
 def read_blob(filename: str):
@@ -10,7 +18,7 @@ def read_blob(filename: str):
     try: 
         blobservice = ContainerClient(account_url = "https://kittsqlmodel.blob.core.windows.net", 
                                         credential= "6n/WfghjW+2xAN2h1iCiYxELJPADDC9h5Fr+iLbg+/1kBSoD8eVSeyeStKEyALWyNRE4XEBT8OJY+ASt5EspHQ==",
-                                        container_name = "sql-outputs")
+                                        container_name = input_container)
         contents = blobservice.get_blob_client(filename).download_blob().readall()
         print(f"Successfully read the file - {filename} from blob container!")
         return contents
@@ -18,25 +26,19 @@ def read_blob(filename: str):
         print(f"Fails to connect to the blob container, the error is {e}.")
 
 
-def create_questions_list(sql: str, filename: str):
-    """Create a list of standardised questions which will be push to OpnAI"""
-    messages = [f"Summarise the SQL query: {sql}" , 
-                f"the SQL query is: {sql}. how the data is flowing?",
-                f"the SQL query is: {sql}. what are the inputs?",
-                f"the SQL query is: {sql}. what are the outputs?",
-                f"the SQL query is: {sql}. what variables are created?",
-                f"the SQL query is: {sql}. what are risks running this query?"]
-    questions = [f"Summarise the SQL query: {filename}" , 
-                f"the SQL query is: {filename}. how the data is flowing?",
-                f"the SQL query is: {filename}. what are the inputs?",
-                f"the SQL query is: {filename}. what are the outputs?",
-                f"the SQL query is: {filename}. what variables are created?",
-                f"the SQL query is: {filename}. what are risks running this query?"]
+def read_questions_list(sql: str, filename: str):
+    """Read a list of standardised questions from the Questions.json fie which will be push to OpnAI"""
+    data = json.load(open("Questions.json"))
+    messages = []
+    questions = []
+    for key, value in data.items():
+        messages.append(f"the SQL query is: {sql}. {value}.")
+        questions.append(f"the SQL query is: {filename}. {value}.")
 
     return messages,questions
 
 
-def connect_openai(filename: str,promots: list, questions: list, ):
+def connect_openai(filename: str,prompts: list, questions: list, ):
     """connect to Azure openai and run promots to retrive the response"""
     file_reviewed = filename[0:filename.find('.')]
     response = {"queryId": file_reviewed, 
@@ -48,9 +50,9 @@ def connect_openai(filename: str,promots: list, questions: list, ):
         client = AzureOpenAI(api_version="2023-05-15",
                             azure_endpoint="https://kmrt.openai.azure.com/",
                             api_key= "a95055f384dd4051a696499d00a064a3")
-        for m in range(0, len(promots)):
+        for m in range(0, len(prompts)):
             completion = client.chat.completions.create( model="cd-kmrt-sbx-gpt4",  
-                                                        messages=[{"role": "user","content": promots[m]},])
+                                                        messages=[{"role": "user","content": prompts[m]},])
             
             resp.append({"questionId": m,
                           questions[m]: completion.choices[0].message.content})
@@ -67,16 +69,14 @@ def connect_openai(filename: str,promots: list, questions: list, ):
 
 def save_reponses(messages: dict):
     """Save the responses to the Cusmos DB"""
-    db_name = "sql-model-review"
-    collection_name = "kitt-collection"
     try:
         CONNECTION_STRING = "mongodb://kitt-sql-review:mPyy7ouKIKP1qZ6PeE636ByyE158Zcq6ErZLOEdMJUNfupAPcJ445jEw0bPPOMhF3rZn9rg7RRsGACDbuyUrUw==@kitt-sql-review.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@kitt-sql-review@"
         mglient = pymongo.MongoClient(CONNECTION_STRING)
-        collection = mglient[db_name][collection_name]
+        collection = mglient[cosmos_db_name][collection_name]
         ## check the collection exists or not
-        if "kitt-collection" not in mglient[db_name].list_collection_names():
+        if "kitt-collection" not in mglient[cosmos_db_name].list_collection_names():
         # Creates a unsharded collection that uses the DBs shared throughput
-            mglient[db_name].command({"customAction": "CreateCollection", "collection": collection_name})
+            mglient[cosmos_db_name].command({"customAction": "CreateCollection", "collection": collection_name})
             print("Created collection '{}'.\n".format(collection_name))
         else:
             print("Using collection: '{}'.\n".format(collection_name))
@@ -90,36 +90,19 @@ def save_reponses(messages: dict):
         print(f"Exporting the responses to Cusmos DB failed because {e}")
 
 
-def main():
-    filename = "GetSizeBand.txt"
+
+def read_sql_query(myblob: func.InputStream):
+    logging.info(f"Python blob trigger function processed blob"
+                f"Name: {myblob.name}"
+                f"Blob Size: {myblob.length} bytes")
+    
+    filename = myblob.name[myblob.name.find("/")+1:]
     sql_string = read_blob(filename)
-    messages,questions = create_questions_list(sql_string, filename)
+    messages,questions = read_questions_list(sql_string, filename)
 
     response = connect_openai(filename, messages,questions)
 
     save_reponses(response)
 
-
-
-
-main()
-
-
-
-
-
-
- 
-
- 
-
-
-
-
-
-
-
-
-    
 
 
